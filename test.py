a0db5e53dab98e0cb3a78e2a0b0ee01fd6b13116
joe souaid <souaid.joe@hotmail.com>
2024-09-27 19:27:40 +0200

    Testing n.2

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-print("hello")
+from collections import defaultdict
 
+input_s = ["act", "pots", "tops", "cat", "stop", "hat"]
+
+
+def anagram(strs: list[str]) -> list[list[str]]:
+    res = defaultdict(list)
+
+    for s in strs:
+        word = [0] * 26
+        for c in s:
+            word[ord(c) - ord('a')] += 1
+
+        res[tuple(word)].append(s)
+
+    return res.values()
+
+
+print(anagram(input_s))
